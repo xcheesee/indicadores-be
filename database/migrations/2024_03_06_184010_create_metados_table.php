56a949b099a351d5397a28cac4b577dd4abb780d
Adinan Batista Alves <adinan0803@gmail.com>
2024-03-12 14:30:10 -0300

    revisão de migrations

--- database/migrations/2024_03_06_184010_create_metados_table.php
+++ database/migrations/2024_03_06_184010_create_metados_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('metadados', function (Blueprint $table) {
             $table->id();
-            $table->string('nome', 45);
+            //$table->string('nome', 45);
             $table->foreignId('tipo_medida_id')->nullable()->constrained('tipo_medidas');
             $table->string('serie_historica_inicio', 45)->nullable();
             $table->string('serie_historica_fim', 45)->nullable();
